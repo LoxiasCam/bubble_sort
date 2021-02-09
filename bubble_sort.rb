f098bf32c2c093841f2d974ca1f8c66d0f6d598f
Camilo <jcamilop96@gmail.com>
2021-02-09 14:53:13 -0500

    add bubble sort by method

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -15,5 +15,24 @@ def bubble_sort(arr)
   arr
 end
 
+def bubble_sort_by(list)
+  i = 0
+  while i < list.length - 1 do
+      comparison = yield(list[i], list[i+1])
+
+    if comparison > 0
+      list[i], list[i+1] = list[i+1], list[i]
+      i = 0
+    else
+      i += 1
+    end
+  end
+  print list
+end
+
+bubble_sort_by(["hi","hello", "hey"]) do |left,right|
+  left.length - right.length
+end
+
 test_array = [1,2,3,4,65,6,3,21,43,65,23,432,313,214,4,54]
 p bubble_sort(test_array)
